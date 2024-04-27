d77568f74fa137594ccda5cae0d1114d3a735ab8
007lazi@gmail.com <007lazi@gmail.com>
2024-04-28 00:49:30 +0200

    speedinsights

--- app/layout.js
+++ app/layout.js
@@ -2,8 +2,8 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import { SpeedInsights } from "@vercel/speed-insights/next"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const OpenSans = Open_Sans({
   weight: ['400', '700'],
