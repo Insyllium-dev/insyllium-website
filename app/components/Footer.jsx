856a8dee03fba015ac9f71167d3b5025a4d752a7
007lazi@gmail.com <007lazi@gmail.com>
2024-05-04 22:50:50 +0200

    po tsho permas

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -34,6 +34,7 @@ const Footer = () => {
         <div className="flex-[1.5] w-full flex flex-row justify-between flex-wrap md:mt-0 mt-10 z-[2]">
           <div className="flex flex-col ss:my-0 my-4 min-w-[150px]">
             <h4 className="font-poppins font-medium text-[20px] leading-[27px] text-white">
+              
               Reach Us
             </h4>
             <ul className="list-none mt-4">
